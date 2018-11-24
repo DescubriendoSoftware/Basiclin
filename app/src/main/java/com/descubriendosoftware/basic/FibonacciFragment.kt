009c47e1bf13eb97e7a13a0967f906e4c2e27f0e
Cabezas <javiergonzalezcabezas@gmail.com>
2018-11-24 12:53:11 +0000

    added new fibonacci

--- app/src/main/java/com/descubriendosoftware/basic/FibonacciFragment.kt
+++ app/src/main/java/com/descubriendosoftware/basic/FibonacciFragment.kt
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fibonacci_fragment.*
+import java.math.BigInteger
 
 class FibonacciFragment : Fragment() {
 
@@ -16,6 +18,17 @@ class FibonacciFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fibonacc_fragment, container, false)
+        var view: View = inflater.inflate(R.layout.fibonacci_fragment, container, false)
+        var result =getFibonacciNumber(4, BigInteger("1"), BigInteger("0"))
+        fibonacci_textView.setText("is$result")
+        return view
+    }
+
+    tailrec fun getFibonacciNumber(n: Int, a: BigInteger, b: BigInteger): BigInteger {
+
+        if (n == 0)
+            return b
+        else
+            return getFibonacciNumber(n - 1, a + b, a)
     }
 }
